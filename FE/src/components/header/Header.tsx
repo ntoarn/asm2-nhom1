8bb81122987247a516e5cfd2f766a39c65be076c
ntoarn <nqton301004@gmail.com>
2024-08-04 14:40:32 +0700

    oke

--- FE/src/components/header/Header.tsx
+++ FE/src/components/header/Header.tsx
@@ -19,10 +19,10 @@ import { Link } from 'react-router-dom';
         <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
           <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search" />
         </form>
-        <div className="text-end">
+        {/* <div className="text-end">
           <button type="button" className="btn btn-outline-light me-2">Login</button>
           <button type="button" className="btn btn-warning">Sign-up</button>
-        </div>
+        </div> */}
         {user ? (
           <>
             <li>
